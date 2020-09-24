957f355977488fc6507c765e83fb89e2c3ed9a77
IOT-8 <71483580+IOT-8@users.noreply.github.com>
2020-09-24 11:28:43 +0530

    Add files via upload

---
+++ enet_echo/cp_tftp.sh
@@ -0,0 +1,2 @@
+echo sudo cp -f ../../../../../../binary/armv7a/gcc/am335x/beaglebone/enet_echo/Release/enetEcho.bin /var/lib/tftpboot/
+sudo cp -f ../../../../../../binary/armv7a/gcc/am335x/beaglebone/enet_echo/Release/enetEcho.bin /var/lib/tftpboot/
