ef6860b43fb0b8e6614eddf621551b2905d80c38
IOT-8 <tejas.ncs@gmail.com>
2020-09-25 12:54:56 +0530

    udpserver

---
+++ udp_serv.c
@@ -0,0 +1,67 @@
+#include "locator.h"
+#include "echod.h"
+#include "interrupt.h"
+#include "lwiplib.h"
+#include "lwipopts.h"
+#include "consoleUtils.h"
+#include "soc_AM335x.h"
+#include "interrupt.h"
+#include "beaglebone.h"
+#include "cache.h"
+#include "delay.h"
+#include "mmu.h"
+#include "cache.h"
+#include "watchdog.h"
+#include "sockets.h"
+
+
+
+  
+#define PORT     8080 
+#define MAXLINE 1024 
+  
+// Driver code 
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from server"; 
+    struct sockaddr_in servaddr, cliaddr; 
+      
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+      
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
+      
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    int len, n; 
+  
+    len = sizeof(cliaddr);  //len is value/resuslt 
+  
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Client : %s\n", buffer); 
+    sendto(sockfd, (const char *)hello, strlen(hello),  
+        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+            len); 
+    printf("Hello message sent.\n");  
+      
+    return 0; 
+} 
